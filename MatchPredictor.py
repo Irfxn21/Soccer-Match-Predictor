a47782084321239e67c4971804470fef87d09167
Irfxn21 <shahid.irfan1107@gmail.com>
2025-09-22 23:16:35 -0400

    creating predictors

--- MatchPredictor.py
+++ MatchPredictor.py
@@ -3,3 +3,9 @@ import pandas as pd
 matches = pd.read_csv("matches.csv", index_col=0)
 
 matches["date"] = pd.to_datetime(matches["date"])
+
+matches["venue_code"] = matches["venue"].astype("category").cat.codes
+matches["opp_code"] = matches["opponent"].astype("category").cat.codes
+matches["hour"] = matches["time"].str.replace(":.+", "", regex=True).astype("int")
+matches["day_code"] = matches["date"].dt.dayofweek
+matches["target"] = (matches["result"] == "W").astype("int")
